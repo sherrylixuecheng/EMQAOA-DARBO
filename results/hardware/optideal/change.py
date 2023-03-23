fbdd9d0790d9df6e5688e7c17b708ad45e572ee5
Lixue Cheng <sherrycheng@tencent.com>
2023-03-23 22:55:15 +0800

    update results

--- results/hardware/optideal/change.py
+++
@@ -1,6 +0,0 @@
-import os
-
-for i in range(1,3):
-	for k in range(5):
-		for j in ['ideal', 'raw', 'mit']:
-			os.system('mv Y_{}_hardware_exp_DARRBO_optideal_100_trial{}_p{}.npy Y_{}_hardware_exp_DARBO_optideal_100_trial{}_p{}.npy'.format(j,k,i,j,k,i))
